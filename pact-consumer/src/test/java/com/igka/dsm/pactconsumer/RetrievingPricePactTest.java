e3e83ad888286bb8cc9c328a6d30e3ca7b70bc3e
lucatodeschini <luca.todeschini@ingka.ikea.com>
2021-04-23 08:49:38 +0200

    pact test

--- pact-consumer/src/test/java/com/igka/dsm/pactconsumer/RetrievingPricePactTest.java
+++ pact-consumer/src/test/java/com/igka/dsm/pactconsumer/RetrievingPricePactTest.java
@@ -20,5 +20,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(PactConsumerTestExt.class)
 public class RetrievingPricePactTest {
+    @Pact(consumer = "PriceCalculator", provider = "ProductCatalog")
+    RequestResponsePact getAllProducts(PactDslWithProvider builder) {
+        return builder.given("product code exists")
+                .uponReceiving("retrieve price")
+                .method("GET")
+                .path("/products/993.283.34/price")
+                .willRespondWith()
+                .status(200)
+                .headers(Map.of("Content-Type", "application/json"))
+                .body(newJsonBody(object -> {                    object.stringType("id", "993.283.34");
+                    object.stringType("name", "APPLARO");
+                    object.stringType("price", "179.83");
+                    object.stringType("currency", "EUR");}).build())
+                .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "getAllProducts")
+    void getProductPrice_whenProductsExist(MockServer mockServer) {
+        Price expectedPrice = new Price("179.83", "EUR");
+
+        RestTemplate restTemplate = new RestTemplateBuilder()
+                .rootUri(mockServer.getUrl())
+                .build();
+
+        Price price = new ProductService(restTemplate).getPrice("993.283.34");
+
+        assertEquals(expectedPrice, price);
+    }
 
 }
