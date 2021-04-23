ceda105f69791eeff988374cd8a96712cd1211bb
lucatodeschini <luca.todeschini@ingka.ikea.com>
2021-04-23 08:57:28 +0200

    producer

---
+++ pact-producer/src/test/kotlin/com/igka/dsm/pactproducer/ProductPricePactProviderTest.java
@@ -0,0 +1,42 @@
+package com.igka.dsm.pactproducer;
+
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@Provider("ProductCatalog")
+@PactBroker(host="localhost", port = "9292")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProductPricePactProviderTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", port));
+        System.setProperty("pact.verifier.publishResults", "true");
+    }
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void verifyPact(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+
+    @State("product code exists")
+    void toProductsCodeExistState() {
+
+    }
+
+}
